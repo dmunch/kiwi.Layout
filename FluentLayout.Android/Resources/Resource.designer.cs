a8f1c1f37efc9aeffd8335f09a0c707fe27178eb
dmunch <dani.munch@gmail.com>
2014-11-05 09:28:19 +0100

    Add submodule references to Cirrious.FluentLayout and Cassowary as well as solution file. Update project references.

--- FluentLayout.Android/Resources/Resource.designer.cs
+++ FluentLayout.Android/Resources/Resource.designer.cs
@@ -41,27 +41,11 @@ namespace FluentLayout.Android
 			}
 		}
 		
-		public partial class Drawable
-		{
-			
-			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
-			
-			static Drawable()
-			{
-				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
-			}
-			
-			private Drawable()
-			{
-			}
-		}
-		
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			// aapt resource value: 0x7f040000
+			public const int myButton = 2130968576;
 			
 			static Id()
 			{
@@ -76,8 +60,8 @@ namespace FluentLayout.Android
 		public partial class Layout
 		{
 			
-			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			// aapt resource value: 0x7f020000
+			public const int Main = 2130837504;
 			
 			static Layout()
 			{
@@ -92,11 +76,11 @@ namespace FluentLayout.Android
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7f030001
+			public const int app_name = 2130903041;
 			
-			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			// aapt resource value: 0x7f030000
+			public const int hello = 2130903040;
 			
 			static String()
 			{
