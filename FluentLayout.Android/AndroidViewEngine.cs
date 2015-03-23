4444e88b7d55d628aee25639cfb22926cf2516d2
dmunch <dani.munch@gmail.com>
2015-03-23 08:55:38 +0100

    * Add FluentLayout, a ViewGroup based on the FluentEngine

---
+++ FluentLayout.Android/AndroidViewEngine.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using FluentLayout.Cassowary;
+using View = Android.Views;
+using Cirrious.FluentLayouts;
+
+namespace FluentLayout.Android
+{
+	public class AndroidViewEngine : IViewEngine<View.View>
+	{
+		#region IViewEngine implementation
+
+		public string GetViewName (View.View view)
+		{
+			return view.Id.ToString();
+		}
+
+		public float GetAttribute (View.View v, LayoutAttribute attribute)
+		{
+			switch (attribute) {
+			case LayoutAttribute.Left:
+				return v.Left;
+				break;
+			case LayoutAttribute.Right:
+				return v.Right;
+				break;
+			case LayoutAttribute.Top:
+				return v.Top;
+				break;
+			case LayoutAttribute.Bottom:
+				return v.Bottom;
+				break;
+			default:
+				throw new NotImplementedException(string.Format("Attribute not implemented: {0}", attribute));
+			}
+		}
+
+		public void SetAttribute (View.View v, LayoutAttribute attribute, float value)
+		{
+			int intVal = (int)value;
+
+			switch (attribute) {
+			case LayoutAttribute.Left:
+				v.Left = intVal;
+				break;
+			case LayoutAttribute.Right:
+				v.Right = intVal;
+				break;
+			case LayoutAttribute.Top:
+				v.Top = intVal;
+				break;
+			case LayoutAttribute.Bottom:
+				v.Bottom = intVal;
+				break;
+			default:
+				throw new NotImplementedException (string.Format ("Attribute not implemented: {0}", attribute));
+			}
+		}
+
+		#endregion
+	}
+
+}
+
---
+++ FluentLayout.Android/FluentLayout.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Android.Content;
+using Android.Views;
+
+using Cirrious.FluentLayouts;
+using FluentLayout.Cassowary;
+using View = Android.Views;
+
+namespace FluentLayout.Android
+{
+	public class FluentLayout : ViewGroup
+	{
+		protected FluentEngine<View.View> fluentEngine = new FluentEngine<View.View>(new AndroidViewEngine());
+		protected List<IFluentLayout<View.View>> fluentLayouts = new List<IFluentLayout<View.View>>();
+
+		public FluentLayout(Context context)
+			:base(context)
+		{
+
+		}
+		protected override void OnLayout (bool changed, int l, int t, int r, int b)
+		{
+			fluentEngine.AddConstraints (this, fluentLayouts);
+			var tableau = fluentEngine.Solve (this);
+
+
+			System.Diagnostics.Debug.WriteLine (tableau);
+			System.Diagnostics.Debug.WriteLine ("l {0} t {1} r {2} b {3}", l, t, r, b);
+			/*
+			for (int childIdx = 0; childIdx < ChildCount; childIdx++) {
+				var child = GetChildAt (childIdx);
+
+				child.Left = 0;
+				child.Top = 0;
+				child.Right = 300;
+				child.Bottom = 300;
+			}*/
+		}
+
+		public void AddConstraints(params IFluentLayout<View.View>[] fluentLayouts)
+		{
+			this.fluentLayouts.AddRange (fluentLayouts);
+		}
+	}
+}
+
