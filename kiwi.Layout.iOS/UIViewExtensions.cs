4c830da5fcd555c84cc8a9bd263ca85486655437
dmunch <dani.munch@gmail.com>
2015-12-10 13:03:44 +0100

    Add nuspec and fix some UIViewExtensions for iOS.

--- kiwi.Layout.iOS/UIViewExtensions.cs
+++ kiwi.Layout.iOS/UIViewExtensions.cs
@@ -2,57 +2,57 @@
 using System.Collections.Generic;
 using UIKit;
 using Foundation;
+using System;
 
-namespace kiwi.Layout.iOS
+namespace kiwi.Layout.Fluent
 {
-	using Fluent;
+    public static class UIViewExtensions
+    {
+        public static void SubviewsDoNotTranslateAutoresizingMaskIntoConstraints(this UIView view)
+        {
+            foreach (var subview in view.Subviews)
+            {
+                subview.TranslatesAutoresizingMaskIntoConstraints = false;
+            }
+        }
 
-	public static class UIViewExtensions
-	{
-		public static void SubviewsDoNotTranslateAutoresizingMaskIntoConstraints(this UIView view)
-		{
-			foreach (var subview in view.Subviews)
-			{
-				subview.TranslatesAutoresizingMaskIntoConstraints = false;
-			}
-		}
+        public static void AddConstraints<T>(this UIView view, params IFluentLayout<T>[] fluentLayouts)
+            where T: UIView
+        {
+            view.AddConstraints(fluentLayouts
+                .Where(fluent => fluent != null)
+                .Select(fluent => fluent.ToLayoutConstraint())
+                .ToArray());
+        }
 
-		public static void AddConstraints<T>(this UIView view, params IFluentLayout<T>[] fluentLayouts)
-			where T: NSObject
-		{
-			view.AddConstraints(fluentLayouts
-				.Where(fluent => fluent != null)
-				.SelectMany(fluent => fluent.ToLayoutConstraints())
-				.ToArray());
-		}
+        public static void AddConstraints<T>(this UIView view, IEnumerable<IFluentLayout<T>> fluentLayouts)
+            where T: UIView
+        {
+            view.AddConstraints(fluentLayouts
+                .Where(fluent => fluent != null)
+                .Select(fluent => fluent.ToLayoutConstraint())
+                .ToArray());
+        }
+    }
 
-		public static void AddConstraints<T>(this UIView view, IEnumerable<IFluentLayout<T>> fluentLayouts)
-			where T: NSObject
-		{
-			view.AddConstraints(fluentLayouts
-				.Where(fluent => fluent != null)
-				.SelectMany(fluent => fluent.ToLayoutConstraints())
-				.ToArray());
-		}
-	}
+    public static class FluentLayoutExtensions
+    {
+        public static NSLayoutConstraint ToLayoutConstraint(this IFluentLayout<UIView> fluentLayout)
+        {
+            var constraint = NSLayoutConstraint.Create (
+                fluentLayout.View,  
+                (NSLayoutAttribute)fluentLayout.Attribute,
+                (NSLayoutRelation)fluentLayout.Relation,
+                fluentLayout.SecondItem != null ? fluentLayout.SecondItem.View : null, // the beauty of C# 6 
+                //no idea however how to deal with value types (e.g. enum)
+                (NSLayoutAttribute)(fluentLayout.SecondItem != null ? fluentLayout.SecondItem.Attribute : LayoutAttribute.NoAttribute),
+                fluentLayout.Multiplier,
+                fluentLayout.Constant);
 
-	public static class FluentLayoutExtensions
-	{
-		public static IEnumerable<NSLayoutConstraint> ToLayoutConstraints(this IFluentLayout<NSObject> fluentLayout)
-		{
-			var constraint = NSLayoutConstraint.Create(
-				fluentLayout.View,
-				(NSLayoutAttribute)((int)fluentLayout.Attribute),
-				(NSLayoutRelation)((int)fluentLayout.Relation),
-				fluentLayout.SecondItem.View,
-				(NSLayoutAttribute)((int)fluentLayout.SecondItem.Attribute),
-				fluentLayout.Multiplier,
-				fluentLayout.Constant);
+            //clamp on 1000
+            constraint.Priority = Math.Min(fluentLayout.Priority, 1000);
 
-			constraint.Priority = fluentLayout.Priority;
-
-			yield return constraint;
-		}
-	}
+            return constraint;
+        }
+    }
 }
-
